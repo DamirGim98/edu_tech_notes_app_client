8761c4d6f13adebfc1184c240cfbc01187c119e8
Damir Gimatdinov <damirgim@yandex.ru>
2022-11-30 17:11:19 +0400

    Changes before deploy

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import App from './App'
 import store from './app/store'
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
